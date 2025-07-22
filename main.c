059219735a5cb433b39659c92a4f0e8c8f0149b5
Ratnesh <ratneshbvk2504@gmail.com>
2025-07-22 19:48:33 +0530

    Forking

--- main.c
+++ main.c
@@ -5,6 +5,38 @@
 #include <stdio.h>
 #include <string.h>
 
+int rtsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0)
+  {
+    // Child process
+    if (execvp(args[0], args) == -1)
+    {
+      perror("rtsh");
+    }
+    exit(EXIT_FAILURE);
+  }
+  else if (pid < 0)
+  {
+    // Error forking
+    perror("rtsh");
+  }
+  else
+  {
+    // Parent process
+    do
+    {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
 char *rtsh_read_line(void)
 {
 #ifdef RTSH_USE_STD_GETLINE
