82fe4acd9c00e816f6f000a69811d83b9e21ac08
Ratnesh <ratneshbvk2504@gmail.com>
2025-07-22 19:00:17 +0530

    Basic loop of a shell

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 #include <stdlib.h>
+
+void rtsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do
+  {
+    printf("> ");
+    line = rtsh_read_line();
+    args = rtsh_split_line(line);
+    status = rtsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
 int main(int argc, char **argv)
 {
   // Load config files, if any.
