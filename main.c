1a684d141a21236c9eb5e4207513b83a85a8100f
Ratnesh <ratneshbvk2504@gmail.com>
2025-07-22 19:43:38 +0530

    Parsing the input

--- main.c
+++ main.c
@@ -71,6 +71,43 @@ char *rtsh_read_line(void)
 #endif
 }
 
+#define RTSH_TOK_BUFSIZE 64
+#define RTSH_TOK_DELIM " \t\r\n\a"
+char **rtsh_split_line(char *line)
+{
+  int bufsize = RTSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token;
+
+  if (!tokens)
+  {
+    fprintf(stderr, "rtsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, RTSH_TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize)
+    {
+      bufsize += RTSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens)
+      {
+        fprintf(stderr, "rtsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, RTSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
 void rtsh_loop(void)
 {
   char *line;
